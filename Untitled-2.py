4f69b9bda084a3f44d350635d568320bcc8459df
qiuguihong <john@qiuguihongs-MacBook-Pro-2.local>
2023-12-03 00:00:04 +0800

    create GLM model

--- Untitled-2.py
+++
@@ -1,7 +0,0 @@
-
-def spline_basis(X, degree=3):
-n_samples, n_features = X.shape
-basis_functions = []
-for i in range(degree):
-basis_functions.append(X ** (i + 1))
-return np.concatenate(basis_functions, axis=1)
--- import pickle.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# 读取.pkl文件
-with open('AgesAndHeights.pkl', 'rb') as file:
-    data = pickle.load(file)
-
-# 使用导入的数据
-print(data)
